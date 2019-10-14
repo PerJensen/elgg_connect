e861ca0aa342b05f918eb3703763aad280a74129
Per Jensen <admin@perjensen-online.dk>
2019-10-14 12:21:00 +0200

    chore(elgg_connect): code cleanup

--- views/default/elgg_connect/elements/featured.php
+++ views/default/elgg_connect/elements/featured.php
@@ -9,26 +9,20 @@ $plugin = elgg_get_plugin_from_id('elgg_connect');
 
 // Get the image
 $img = elgg_get_simplecache_url('graphics/featured.jpg');
+			
+$cover = elgg_format_element('div', [
+	'class' => 'slide',
+	'style' => "background-image: url('{$img}');"
+]);
 
-?>
+$image = elgg_format_element('div', ['class' => 'elgg-featured-image'], $cover);
 
-<div class="elgg-featured-container">
-	<div class="elgg-featured">
-		<div class="featured-image">
-			<?php 				
-				echo "<div class=\"slide\" style=\"
-					background-image: url(" . $img . ");
-					background-repeat: no-repeat; 
-					background-position: center center;
-					background-size: cover;
-				\"></div>";
-			?>
-		</div>
-		<div class="ez-caption">
-			<div class="elgg-inner">			
-				<h1><?php echo $plugin->caption_h1; ?></h1>
-				<h2><?php echo $plugin->caption_h2; ?></h2>
-			</div>
-		</div>
-	</div>
-</div>
+$h1 = elgg_format_element('h1', [], $plugin->caption_h1);
+$h2 = elgg_format_element('h2', [], $plugin->caption_h2);
+
+$inner = elgg_format_element('div', ['class' => 'elgg-inner'], $h1 . $h2);
+$content = elgg_format_element('div', ['class' => 'elgg-caption'], $inner);
+
+echo elgg_format_element('div', [
+	'class' => 'elgg-featured-container'
+], $image . $content);
--- views/default/page/layouts/front_page.php
+++ views/default/page/layouts/front_page.php
@@ -26,84 +26,88 @@ $members = $vars['members'];
 
 echo elgg_view('elgg_connect/elements/featured');
 
-?>
-<div class="elgg-landing-page-body">
-	<?php if ($plugin->display_action == "yes") { ?>
-	<div class="elgg-layout-content">
-		<div class="elgg-container action">
-			<div class="elgg-head elgg-layout-header">
-				<h1><?php echo $plugin->action_h1; ?></h1>			
-			</div>
-			<div class="elgg-layout-content">
-				<?php echo $plugin->action_h2; ?>
-			</div>
-		</div>
-	</div>
-	<?php } ?>
-	<?php if ($plugin->display_members == "yes") { ?>
-	<div class="elgg-layout-content">
-		<div class="elgg-container users">
-			<div class="elgg-head elgg-layout-header">	
-				<h1><?php echo $members_url; ?></h1>
-			</div>			
-			<?php
-			$i = 0; 
-			foreach ($members as $member) {
-				if ($member->banned == 'yes') {
-					continue;
-				}
-				$name = elgg_format_element('p', [], $member->name);
-				echo elgg_format_element('div', [
-					'class' => 'elgg-item',
-				], elgg_view_entity_icon($member, 'medium', [
-					'use_hover' => false
-				]) . $name);
-				
-				$i ++;
-				if ($i == 5) {
-					break;
-				}
-			} 
-			?>
-		</div>
-	</div>
-	<?php } ?>
-	<?php if ($plugin->display_groups == "yes") { ?>
-	<div class="elgg-layout-content">
-		<div class="elgg-container groups">
-			<div class="elgg-head elgg-layout-header">
-				<h1><?php echo $groups_url; ?></h1>
-			</div>
-			<?php
-			$i = 0;
-			foreach ($groups as $group) {
-				if ($group->getContentAccessMode() == ElggGroup::CONTENT_ACCESS_MODE_MEMBERS_ONLY) {
-					continue;
-				}
-				$image = elgg_format_element('div', [
-					'class' => 'elgg-image',
-				], elgg_view_entity_icon($group, 'group_cover_small'));					
-				
-				$url = elgg_view('output/url', array(
-					'text' => $group->name,
-					'href' => $group->getURL()
-				));
-				$title = elgg_format_element('h2', [], $url);
-				
-				$members = elgg_echo('members') . ": " . $group->getMembers(array('count' => true));	
-				$sub = elgg_format_element('div', [], $members);
-								
-				$body = elgg_format_element('div', ['class' => 'elgg-body'], $title . $sub);
-				
-				echo elgg_format_element('div', ['class' => 'elgg-image-block'], $image . $body);
-				
-				$i ++;
-				if ($i == 3) {
-					break;
-				}
-			}				
-			?>
-		</div>
-	</div>
-	<?php } ?>
-</div>
+// action content
+if ($plugin->display_action == "yes") {
+			
+	$h1 = elgg_format_element('h1', [], $plugin->action_h1);
+	$header = elgg_format_element('div', [
+		'class' => 'elgg-head elgg-layout-header',
+	], $h1);
+	$sub = elgg_format_element('div', [
+		'class' => 'elgg-layout-content',
+	], $plugin->action_h2);
+	
+	$body = elgg_format_element('div', ['class' => 'elgg-container action'], $header . $sub);
+	$action_content = elgg_format_element('div', ['class' => 'elgg-layout-content'], $body);
+
+}
+// members list 
+if ($plugin->display_members == "yes") {
+		
+	$url = elgg_format_element('h1', [], $members_url); 
+	$header = elgg_format_element('div', [
+		'class' => 'elgg-head elgg-layout-header',
+	], $url);
+	
+	$i = 0; 
+	foreach ($members as $member) {
+		if ($member->banned == 'yes') {
+			continue;
+		}
+		$name = elgg_format_element('p', [], $member->name);
+		$items .= elgg_format_element('div', [
+			'class' => 'elgg-item',
+		], elgg_view_entity_icon($member, 'medium', [
+			'use_hover' => false
+		]) . $name);
+		
+		$i ++;
+		if ($i == 5) {
+			break;
+		}
+	}
+	$body = elgg_format_element('div', ['class' => 'elgg-container users'], $header . $items);
+	$users_content = elgg_format_element('div', ['class' => 'elgg-layout-content'], $body);
+}
+// groups list
+if ($plugin->display_groups == "yes") {
+					
+	$url = elgg_format_element('h1', [], $groups_url);
+	$header = elgg_format_element('div', [
+		'class' => 'elgg-head elgg-layout-header',
+	], $url);
+
+	$i = 0;
+	foreach ($groups as $group) {
+		if ($group->getContentAccessMode() == ElggGroup::CONTENT_ACCESS_MODE_MEMBERS_ONLY) {
+			continue;
+		}
+		$image = elgg_format_element('div', [
+			'class' => 'elgg-image',
+		], elgg_view_entity_icon($group, 'group_cover_small'));		
+		
+		$url = elgg_view('output/url', array(
+			'text' => $group->name,
+			'href' => $group->getURL()
+		));
+		$title = elgg_format_element('h2', [], $url);
+		
+		$members = elgg_echo('members') . ": " . $group->getMembers(array('count' => true));	
+		$sub = elgg_format_element('div', [], $members);
+						
+		$body = elgg_format_element('div', ['class' => 'elgg-body'], $title . $sub);
+		
+		$group_item .= elgg_format_element('div', ['class' => 'elgg-image-block'], $image . $body);
+		
+		$i ++;
+		if ($i == 3) {
+			break;
+		}
+	}
+	$body = elgg_format_element('div', ['class' => 'elgg-container groups'], $header . $group_item);
+	$groups_content = elgg_format_element('div', ['class' => 'elgg-layout-content'], $body);
+}
+	
+echo elgg_format_element('div', [
+	'class' => 'elgg-landing-page-body'
+], $action_content . $users_content . $groups_content);
